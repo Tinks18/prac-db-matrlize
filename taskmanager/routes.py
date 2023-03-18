f9541198b06c67a7d686f3d73dbdd91add8e981c
Tinks18 <tinutani9@gmail.com>
2023-03-18 04:56:21 +0000

    update tasks

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -5,10 +5,11 @@ from taskmanager.models import Category, Task
 
 @app.route("/")
 def home():
-    return render_template("tasks.html")
+    tasks = list(Task.query.order_by(Task.id).all())
+    return render_template("tasks.html", tasks=tasks)
 
 
-@app.route("/Categories")
+@app.route("/categories")
 def categories():
     categories = list(Category.query.order_by(Category.category_name).all())
     return render_template("categories.html", categories=categories)
@@ -20,7 +21,7 @@ def add_category():
         category = Category(category_name=request.form.get("category_name"))
         db.session.add(category)
         db.session.commit()
-        return redirect(url_for("categories"))    
+        return redirect(url_for("categories"))
     return render_template("add_category.html")
 
 
@@ -30,11 +31,11 @@ def edit_category(category_id):
     if request.method == "POST":
         category.category_name = request.form.get("category_name")
         db.session.commit()
-        return redirect(url_for("categories"))    
+        return redirect(url_for("categories"))
     return render_template("edit_category.html", category=category)
 
 
-@app.route("/delete_category/<int:category_id>", methods=["GET", "POST"])
+@app.route("/delete_category/<int:category_id>")
 def delete_category(category_id):
     category = Category.query.get_or_404(category_id)
     db.session.delete(category)
@@ -52,8 +53,22 @@ def add_task():
             is_urgent=bool(True if request.form.get("is_urgent") else False),
             due_date=request.form.get("due_date"),
             category_id=request.form.get("category_id")
-           )
+        )
         db.session.add(task)
         db.session.commit()
-        return redirect(url_for("home"))    
+        return redirect(url_for("home"))
     return render_template("add_task.html", categories=categories)
+
+
+@app.route("/edit_task/<int:task_id>", methods=["GET", "POST"])
+def edit_task(task_id):
+    task = Task.query.get_or_404(task_id)
+    categories = list(Category.query.order_by(Category.category_name).all())
+    if request.method == "POST":
+        task.task_name = request.form.get("task_name")
+        task.task_description = request.form.get("task_description")
+        task.is_urgent = bool(True if request.form.get("is_urgent") else False)
+        task.due_date = request.form.get("due_date")
+        task.category_id = request.form.get("category_id")
+        db.session.commit()
+    return render_template("edit_task.html", task=task, categories=categories)
